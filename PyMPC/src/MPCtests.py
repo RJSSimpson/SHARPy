25d362d833fc134f582c26b50e575295e8d83fd0
RJSSimpson <r.simpson11@imperial.ac.uk>
2013-08-21 17:12:53 +0100

    Completed problem definition for muAO-mpc.

--- PyMPC/src/MPCtests.py
+++ PyMPC/src/MPCtests.py
@@ -220,7 +220,9 @@ if __name__ == '__main__':
     
     # Define state-space system.
     dSys = StateSpace(A,B,C,D) # First create a continuous time.
-    dSys.cont2discrete(0.5) # Convert to discrete.
+    
+    dt = 0.5
+    dSys.cont2discrete(dt) # Convert to discrete.
     
     # begin defining control using muaompc
     # http://ifatwww.et.uni-magdeburg.de/syst/research/muAO-MPC/doc/html/tutor.advanced.html
@@ -229,15 +231,43 @@ if __name__ == '__main__':
     mu = 100
     
     # TODO: Automatically append auxiliary states for slew rates.
-    Ad = np.array(dSys.A, copy=True)
-    Ad.resize((Ad.shape[0]+1, Ad.shape[1]))
+    Ad = np.zeros((5,5))
+    Ad[:Ad.shape[0]-1,:Ad.shape[1]-1] = np.array(dSys.A, copy=True)
     
     Bd = np.array(dSys.B, copy=True)
     Bd.resize((Bd.shape[0]+1, Bd.shape[1]))
     Bd[dSys.nX():,:] = np.eye(dSys.nU())
     
-    print(Ad,Bd)
+    # Weighting matrices
+    Q = np.eye(Ad.shape[0])
+    Q[4,4] = 0.0 #Auxiliary state for slew rates
+    P = Q # Terminal constraint
+    R = np.eye(Bd.shape[1])
     
+    # Input constraints
+    eui = 15.0*np.pi/180.0
+    u_lb = [[-eui]]
+    u_ub = [[eui]]
     
+    # Mixed constraints
+    ex2 = 20.0*np.pi/180.0
+    ex5 = 30*np.pi/180.0*dt # Maximum change in control input each time step.
+    ey3 = 30.0 # Altitude rate limit, ms-1.
+    e_lb = [[-ex2], [-ey3], [-ex5]]
+    e_ub = [[ex2], [ey3], [ex5]]
     
+    # Mixed constraint matrices
+    Kx = [[     0,      1,      0,      0,      0],
+          [-128.2,  128.2,      0,      0,      0],
+          [     0.,     0.,     0.,     0.,    -1.]]
+    Ku = [[0],
+          [0],
+          [1]]
+    
+    # Terminal constraints
+    f_lb = e_lb
+    f_ub = e_ub
+    F = Kx # Terminal constraint matrix
+    
+    # set-up MPC problem
     
