2cd30a4fe4e7e4067c45a5acc3835b873d356081
RJSSimpson <r.simpson11@imperial.ac.uk>
2013-09-11 17:26:36 +0100

    Allow system or string specification in lin dynamic siulation.

--- src/PyCoupled/Coupled_LinDynamic.py
+++ src/PyCoupled/Coupled_LinDynamic.py
@@ -11,10 +11,15 @@ import numpy as np
 def Solve_Py(*args, **kwords):
     """@brief time domain solution of linear aeroelastic system.
     args:
-        * 4 (matPath, U, t, x0)
-        * 5(+) NotImplemented (TODO: write a linearization routine.)
+        * 4 (matPath/(A,B,C,D,dt), U, t, x0)
         
-    @param matPath string containing absolute path of .mat file with disSys struct.
+    @param matPath string containing absolute path of .mat file with disSys
+            struct.
+    @param A state space system matrix.
+    @param B state space system matrix.
+    @param C state space system matrix.
+    @param D state space system matrix.
+    @param dt Timestep.
     @param U u(t), the input sequence. can be zeros to simulate free response.
     @param t timesteps at which input is defined (None to default).
     @param x0 The initial condition on the state vector(None: zero by default).
@@ -25,24 +30,46 @@ def Solve_Py(*args, **kwords):
     
     if len(args) == 4:
         # Initialize discrete time system
-        disSys = StateSpace(args[0])
+        if isinstance(args[0], str):
+            disSys = StateSpace(args[0])
+        elif isinstance(args[0], tuple):
+            disSys = StateSpace(args[0][0],
+                                args[0][1],
+                                args[0][2],
+                                args[0][3],
+                                args[0][4])
         # check input sequence
         if disSys.nU() != args[1].shape[1]:
             raise ValueError("Wrong number of inputs in input sequence")
-        
         # Run discrete time sim
-        dlsim((disSys.A,disSys.B,disSys.C,disSys.D,disSys._Ts),
-              U,
-              t = args[2],
-              x0 = args[3])
-        
-    elif len(args) >= 5:
-        raise NotImplementedError("Linearization not available in SHARPy yet.")
+        tout, yout, xout = dlsim((disSys.A,disSys.B,
+                                  disSys.C,disSys.D,
+                                  disSys._Ts),
+                                  U,
+                                  t = args[2],
+                                  x0 = args[3])
+        return tout, yout, xout
+    else:
+        raise ValueError("Needs 4 input arguments")
     
     
 if __name__ == '__main__':
     
     matPath = '/home/rjs10/Documents/MATLAB/MPC/HALE/HALE_sigma1000_Py'
-    U = np.zeros((2,4)) # HALE example, no thrust
+    U = np.zeros((1,4)) # HALE example, no thrust
     
     Solve_Py(matPath,U,None,None)
+    
+    # Define system
+    A=np.array([[1]])
+    B=np.array([[1,1,1]])
+    C=np.array([[1]])
+    D=np.array([[0,0,0]])
+    dt = 1.0
+    
+    # Define input
+    U=np.array([[0,0,0]]) # Input tensor with 1 timestep.
+    
+    dlsim((A,B,C,D,dt),U)
+    
+    Solve_Py((A,B,C,D,dt),U,None,None)
