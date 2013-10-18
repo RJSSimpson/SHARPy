Fixed bug in calculation of normal vector in UVLMLib which only can into effect due to complex panel geometries on the edge of control surfaces.

input/Goland.py
@@ -5,6 +5,7 @@ import PyCoupled.Utils.DerivedTypesAeroelastic as DerivedTypesAeroelastic
 import ctypes as ct
 import numpy as np
 from PyBeam.Utils.XbeamLib import Skew
+from collections import OrderedDict
 
 # Beam options.
 XBOPTS = DerivedTypes.Xbopts(FollowerForce = ct.c_bool(False),
@@ -91,21 +92,20 @@ VMINPUT = DerivedTypesAero.VMinput(c = c,
                                    U_mag = Umag,
                                    alpha = 0.0*np.pi/180.0,
                                    theta = 0.0,
-                                   ZetaDotTest = 0.0,
                                    WakeLength = WakeLength,
                                    ctrlSurf = ctrlSurf)
 # Unsteady aero inputs.
 VelA_G   = np.array(([0.0,0.0,0.0]))
 OmegaA_G = np.array(([0.0,0.0,0.0]))
-VMUNST   = VMCoupledUnstInput(VMOPTS, VMINPUT, 0.0, 0.0,
-                              VelA_G, OmegaA_G)
+VMUNST   = DerivedTypesAero.VMCoupledUnstInput(VMOPTS, VMINPUT, 0.0, 0.0,
+                                               VelA_G, OmegaA_G)
 
 # Aerolastic simulation results.
-AELAOPTS = AeroelasticOps(ElasticAxis = ElasticAxis,
-                          InertialAxis = InertialAxis,
-                          AirDensity = 1.02,
-                          Tight = False,
-                          ImpStart = False)
+AELAOPTS = DerivedTypesAeroelastic.AeroelasticOps(ElasticAxis = ElasticAxis,
+                                                  InertialAxis = InertialAxis,
+                                                  AirDensity = 1.02,
+                                                  Tight = False,
+                                                  ImpStart = False)
 
 # Live output options.
 writeDict = OrderedDict()

src/PyAero/UVLM/Solver/VLM.py
@@ -216,27 +216,40 @@ def Run_Cpp_Solver_VLM(VMOPTS, VMINPUT, VMUNST = None, AELOPTS = None):
     # close file
     PostProcess.CloseAeroTecFile(FileObject)
     
+    if Settings.WriteUVLMdebug == True:
+        debugFile = Settings.OutputDir + 'gamma_1degBeta.dat'
+        np.savetxt(debugFile, Gamma.flatten('C'))
+    
     # post process to get coefficients
     return PostProcess.GetCoeffs(VMOPTS, Forces, VMINPUT, VMUNST.VelA_G)
 
 
 if __name__ == '__main__':
     # Create inputs
-    M = 16
-    N = 28
-    Mstar = 1
-    VMOPTS = VMopts(M,N,False,Mstar,True,True)
+    from Goland import *
     
-    # define wing geom
-    c = 1.0
-    b = 1000 #semi-span
+    # Re-define control Surface
+    ctrlSurf = DerivedTypesAero.ControlSurf(iMin = M - M/4,
+                                            iMax = M,
+                                            jMin = N - N/4,
+                                            jMax = N,
+                                            typeMotion = 'cos',
+                                            betaBar = 1.0*np.pi/180.0,
+                                            omega = 0.0)
+    # Inputs.
+    WakeLength = 100.0
+    VMINPUT = DerivedTypesAero.VMinput(c = c,
+                                   b = XBINPUT.BeamLength,
+                                   U_mag = Umag,
+                                   alpha = 0.0*np.pi/180.0,
+                                   theta = 0.0,
+                                   WakeLength = WakeLength,
+                                   ctrlSurf = ctrlSurf)
     
-    # free stream conditions
-    U_mag = 25.0
-    alpha = 1.0*np.pi/180.0
-    theta = 0.0*np.pi/180.0
-    VMINPUT = VMinput(c, b, U_mag, alpha, theta)
-    
+    # Redefine VMOPTS
+    VMOPTS.Mstar = ct.c_int(1)
+    VMOPTS.Steady = ct.c_bool(True)
+    VMOPTS.ImageMethod = ct.c_bool(True)
     # run solver
     Coeffs = Run_Cpp_Solver_VLM(VMOPTS,VMINPUT)
     

src/PyAero/UVLM/Utils/UVLMLib.py
@@ -35,6 +35,13 @@ cpp_test_biotsegment.restype = None
 c_test_biotsegment.restype = None
 cpp_solver_vlm.restype = None
 
+def AreEqual(arg1,arg2):
+    """@brief Returns true if argumants are equal."""
+    if arg1 == arg2:
+        return True
+    else:
+        return False
+
 def Cpp_Vorticity_BiotSegment(xP,x1,x2,Gamma,Uind):
     """@brief Wrapper for c_vorticity_biotsegment."""
     
@@ -82,7 +89,7 @@ def Cpp_Solver_VLM(Zeta, ZetaDot, Uext, ZetaStar, VMOPTS, Forces, \
     """@details wrapper for cpp_solver_vlm."""
     
     "If memory for AIC and BIC is not allocated, allocate here."
-    if AIC == None or BIC == None:
+    if AIC == None and BIC == None:
         AIC = np.zeros((VMOPTS.M.value*VMOPTS.N.value,\
                         VMOPTS.M.value*VMOPTS.N.value), \
                         ct.c_double,'C')
@@ -108,7 +115,7 @@ def Cpp_Solver_VLM(Zeta, ZetaDot, Uext, ZetaStar, VMOPTS, Forces, \
                    Gamma.ctypes.data_as(ct.POINTER(ct.c_double)), \
                    GammaStar.ctypes.data_as(ct.POINTER(ct.c_double)), \
                    AIC.ctypes.data_as(ct.POINTER(ct.c_double)), \
-                   BIC.ctypes.data_as(ct.POINTER(ct.c_double)))
+                   BIC.ctypes.data_as(ct.POINTER(ct.c_double))) 
     
 
 if __name__ == '__main__':

src/PyCoupled/Coupled_NlnDynamic.py
@@ -749,7 +749,7 @@ if __name__ == '__main__':
     jMax = N
     typeMotion = 'sin'
     betaBar = 1.0*np.pi/180.0
-    omega = 15.0
+    omega = 30.0
     ctrlSurf = ControlSurf(iMin,
                            iMax,
                            jMin,
@@ -762,7 +762,6 @@ if __name__ == '__main__':
                                        U_mag = Umag,
                                        alpha = 0.0*np.pi/180.0,
                                        theta = 0.0,
-                                       ZetaDotTest = 0.0,
                                        WakeLength = WakeLength,
                                        ctrlSurf = ctrlSurf)
     # Unsteady aero inputs.
@@ -776,7 +775,7 @@ if __name__ == '__main__':
                               InertialAxis = InertialAxis,
                               AirDensity = 1.02,
                               Tight = False,
-                              ImpStart = True)
+                              ImpStart = False)
     
     # Live output options.
     writeDict = OrderedDict()
@@ -786,7 +785,7 @@ if __name__ == '__main__':
     writeDict['M_z (root)'] = 0
     
     Settings.OutputDir = Settings.SharPyProjectDir + "output/MPC/Goland/testControl/"
-    Settings.OutputFileRoot = "3noded_15rads_KJ"
+    Settings.OutputFileRoot = "3noded_30rads_KJ"
     
     # Solve nonlinear dynamic simulation.
     Solve_Py(XBINPUT, XBOPTS, VMOPTS, VMINPUT, AELAOPTS,